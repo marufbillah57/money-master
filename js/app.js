a758c2b77ca53e3898539bea4e992f11d286636e
marubillah57 <marufbillah5157@gmail.com>
2022-02-17 01:17:29 +0600

    expenses cost added section

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
-console.log('connect js');
+
+
+document.getElementById('calculate').addEventListener('click', function(){
+    const incomeInput = document.getElementById('income-input');
+    const incomeAmount = parseFloat(incomeInput.value);
+    // expenses 
+    const food = document.getElementById('food-input');
+    const foodAmount = parseFloat(food.value);
+    const rent = document.getElementById('rent-input');
+    const rentAmount = parseFloat(rent.value);
+    const clothes = document.getElementById('clothes-input');
+    const clothesAmount = parseFloat(clothes.value);
+    console.log(clothesAmount);
+    balanceCalculate();
+    
+});
+
+
+function balanceCalculate(){
+    const food = document.getElementById('food-input');
+    const foodAmount = parseFloat(food.value);
+    const rent = document.getElementById('rent-input');
+    const rentAmount = parseFloat(rent.value);
+    const clothes = document.getElementById('clothes-input');
+    const clothesAmount = parseFloat(clothes.value);
+    const totalCost = foodAmount + rentAmount + clothesAmount;
+    document.getElementById('total-expense').innerText = totalCost;
+}
