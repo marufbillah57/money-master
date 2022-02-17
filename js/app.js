cdfbeb4a059cd8c0389e69483520e59413754e68
marubillah57 <marufbillah5157@gmail.com>
2022-02-17 20:20:19 +0600

    comment updated

--- js/app.js
+++ js/app.js
@@ -1,57 +1,6 @@
-// function getProductId(idName){
-//     const product = document.getElementById(idName);
-//     const productNumber = parseFloat(product.value);
-//     // clear input field 
-//     product.value = '';
-//     if(productNumber > 0){
-//         return productNumber;
-//     }
-   
-// }
 
-// document.getElementById('calculate-button').addEventListener('click', function(){
-//    const incomeAmount = getProductId('income-input');
-//    const foodAmount = getProductId('food-input');
-//    const rentAmount = getProductId('rent-input');
-//    const clothesAmount = getProductId('clothes-input');
-//    // Expenses total  
-//    const totalCost = foodAmount + rentAmount + clothesAmount;
-//    if(incomeAmount > totalCost){
-//         document.getElementById('total-expense').innerText = totalCost;
-//    } else {
-//        alert('Please enter a valid number for totalcost');
-//    }
-
-//    // Balance update
-//    if(incomeAmount > totalCost){
-//     document.getElementById('balance').innerText = incomeAmount - totalCost;
-//    } else {
-//     //    alert('Please input valid number!');
-//    }
-  
-    
-// });
-
-// document.getElementById('save-button').addEventListener('click', function(){
-//     const saveAmountText = document.getElementById('saving-amount');
-//     const remainAmountText = document.getElementById('remaining-balance').innerText;
-//     const incomeAmount = getProductId('income-input');
-//     const saveAmount = (incomeAmount * 10) / 100;
-//     saveAmountText.innerText = saveAmount;
-// });
-   
-
-
-
+// event handler calculataion
 document.getElementById('calculate-button').addEventListener('click', function(){
-	// const incomeInput = document.getElementById('income-input');
-	// const incomeAmount = parseFloat(incomeInput.value);
-	// const foodInput = document.getElementById('food-input');
-	// const foodAmount = parseFloat(foodInput.value);
-	// const rentInput = document.getElementById('rent-input');
-	// const rentAmount = parseFloat(rentInput.value);
-	// const clothesInput = document.getElementById('clothes-input');
-	// const clothesAmount = parseFloat(clothesInput.value);
 
 	balanceUpdate();
 	
